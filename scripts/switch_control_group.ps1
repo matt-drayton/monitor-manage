6bfed06cc69a7e9d2dd78aec6db852073d546a96
Matt Drayton <matthewdrayton@outlook.com>
2025-09-23 18:43:35 +0100

    done

--- scripts/switch_control_group.ps1
+++ scripts/switch_control_group.ps1
@@ -2,7 +2,25 @@ param(
     [Parameter(Mandatory=$true)][string]$controlGroup
 )
 
-$audioDeviceName = (Get-Content -Path  "$([Environment]::GetFolderPath("MyDocuments"))/monitor-manage/config.json" | ConvertFrom-Json).$controlGroup.audio
+$config = (Get-Content -Path  "$([Environment]::GetFolderPath("MyDocuments"))/monitor-manage/config.json" | ConvertFrom-Json)
+
+# Set audio device
+$audioDeviceName = $config.$controlGroup.audio
 $audioDeviceIndex = (Get-AudioDevice -List | Where-Object { $_.Name -eq $audioDeviceName }).Index
 
 Set-AudioDevice -Index $audioDeviceIndex
+
+# Enable and disable monitors
+$displaysToEnable = $config.$controlGroup.activeDisplays
+$displaysToDisable = $config.$controlGroup.disableDisplays
+
+foreach ($display in $displaysToEnable) {
+    $displayIndex = (Get-DisplayInfo | Where-Object {$_.DisplayName -eq $display}).DisplayId
+    Enable-Display $displayIndex
+}
+
+
+foreach ($display in $displaysToDisable) {
+    $displayIndex = (Get-DisplayInfo | Where-Object {$_.DisplayName -eq $display}).DisplayId
+    Disable-Display $displayIndex
+}
